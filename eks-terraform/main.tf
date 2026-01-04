a5befa8dd644cab491885af65e1f5187d0fc8410
mbose360 <moatazbillahgahgah@gmail.com>
2026-01-04 12:58:23 +0100

    cerrected

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -2,8 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-
-
 # ----------------------------
 # VPC and Subnet Data Sources
 # ----------------------------
@@ -42,8 +40,8 @@ data "aws_security_group" "selected" {
 # ----------------------------
 resource "aws_eks_cluster" "eks" {
   name     = "project-eks"
-  role_arn = arn:aws:iam::381492192944:role/c191399a4934151l13165541t1w381492-LabEksClusterRole-7kwEcmC4VNc8
-}
+  role_arn = "arn:aws:iam::381492192944:role/c191399a4934151l13165541t1w381492-LabEksClusterRole-7kwEcmC4VNc8"
+
   vpc_config {
     subnet_ids         = [data.aws_subnet.subnet-1.id, data.aws_subnet.subnet-2.id]
     security_group_ids = [data.aws_security_group.selected.id]
@@ -56,14 +54,13 @@ resource "aws_eks_cluster" "eks" {
   }
 }
 
-
 # ----------------------------
 # EKS Node Group
 # ----------------------------
 resource "aws_eks_node_group" "node-grp" {
   cluster_name    = aws_eks_cluster.eks.name
   node_group_name = var.node_group_name
-  node_role_arn   = arn:aws:iam::381492192944:role/c191399a4934151l13165541t1w381492192-LabEksNodeRole-dQCsfCxbmRW1
+  node_role_arn   = "arn:aws:iam::381492192944:role/c191399a4934151l13165541t1w381492192-LabEksNodeRole-dQCsfCxbmRW1"
   subnet_ids      = [data.aws_subnet.subnet-1.id, data.aws_subnet.subnet-2.id]
   capacity_type   = "ON_DEMAND"
   disk_size       = 20
@@ -86,6 +83,5 @@ resource "aws_eks_node_group" "node-grp" {
   update_config {
     max_unavailable = 1
   }
-
 }
 
